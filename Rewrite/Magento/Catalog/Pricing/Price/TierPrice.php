3ee93227f243e2b80462d592e5310a9b834078c6
EC2 Default User <ec2-user@ip-172-31-77-115.ec2.internal>
2021-03-29 05:41:54 +0000

    initial commit

---
+++ Rewrite/Magento/Catalog/Pricing/Price/TierPrice.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Genaker\TierPrice\Rewrite\Magento\Catalog\Pricing\Price;
+
+class TierPrice extends \Magento\Catalog\Pricing\Price\TierPrice
+{
+
+     /**
+     * Get price value
+     *
+     * @return bool|float
+     */
+    public function getValue()
+    {
+	$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+	$request = $objectManager->get('\Magento\Framework\App\Request\Http');
+
+	if ($request->getFullActionName() == 'catalog_category_view'){
+	 return null;
+	}
+	
+	return parent:getValue();
+    }
+}
+
