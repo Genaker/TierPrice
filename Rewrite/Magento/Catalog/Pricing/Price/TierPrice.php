6924cdb1d1e825007e225d72cc654a5053db28a0
Egor Shitikov <egorshitikov@gmail.com>
2021-05-10 21:20:19 -0700

    Fig logged in Tier price

--- Rewrite/Magento/Catalog/Pricing/Price/TierPrice.php
+++ Rewrite/Magento/Catalog/Pricing/Price/TierPrice.php
@@ -19,12 +19,12 @@ class TierPrice extends \Magento\Catalog\Pricing\Price\TierPrice
     {
 	$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 	$request = $objectManager->get('\Magento\Framework\App\Request\Http');
+	$session = $objectManager->get('\Magento\Customer\Model\Session');
 
-	if ($request->getFullActionName() == 'catalog_category_view'){
+	if ($request->getFullActionName() == 'catalog_category_view' && !$session->isLoggedIn()){
 	 return false;
 	}
 	
 	return parent:getValue();
     }
 }
-
